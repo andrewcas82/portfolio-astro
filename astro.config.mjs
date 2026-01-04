4e559bfa08e9a45142cd0cc9ae4918d2bd0a4bf3
andrewcas82 <67315875+andrewcas82@users.noreply.github.com>
2026-01-04 16:50:00 +0400

    Fix Astro site URL for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "andres-castrillon-portafolio.vercel.app",
+    site: "https://andrescasportafolio.vercel.app",
 });
